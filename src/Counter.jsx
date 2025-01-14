beae3f574cab292f010de3a6c0b0d17e62435cee
maansi <maansi.bmninfotech13@gmail.com>
2025-01-14 22:08:16 +0530

    Fixes in counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 function Counter() {
     const [counter, setCounter] = useState(1);
-    const decCounter = () => setCounter(counter - 1)
-    const incCounter = () => setCounter(counter + 1)
+    const decCounter = () => counter > 0 ? setCounter(counter - 1) : ''
+    const incCounter = () => counter < 20 ? setCounter(counter + 1) : ''
 
     return <>
         <button onClick={decCounter}>-</button>
         <h1>{counter}</h1>
-        <button onClick={incCounter}>-</button>
+        <button onClick={incCounter}>+</button>
     </>
 }
 
